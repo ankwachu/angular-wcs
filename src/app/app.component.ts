9211faf2b5814292de21f11f051ac223edd5078b
Anthony Kwan Chung <anthonykwanchung@gmail.com>
2020-04-10 12:30:32 +0200

    Customs Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Anthony KWAN CHUNG';
+  title = 'Anthony KWAN CHUNG'
+  displayShowMovies: boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
+import { ShowMoviesDirective } from './show-movies.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective,
+    ShowMoviesDirective
   ],
   imports: [
     BrowserModule,
