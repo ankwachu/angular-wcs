3878c73d7cb0260771a77f0b2189a1073328e8e5
Anthony Kwan Chung <anthonykwanchung@gmail.com>
2020-04-29 21:49:59 +0200

    refactor select option

--- src/app/components/search-movie/search-movie.component.ts
+++ src/app/components/search-movie/search-movie.component.ts
@@ -10,32 +10,53 @@ import { rangeDateValidator } from 'src/app/validators/rangeDateValidator';
   styleUrls: ['./search-movie.component.css']
 })
 export class SearchMovieComponent implements OnInit {
-
-  myForm: FormGroup;
-  minDate: Date = new Date(1940, 0, 1);
+  myForm: FormGroup
+  minDate: Date = new Date(1900, 0, 1);
   maxDate: Date = new Date();
+  types = [];
+  fiches = [];
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
       id: this.fb.group({
         identifiant: [''],
         titre: ['']
       }, { validator: isRequiredValidator('identifiant', 'titre') }),
-      type: ['serie'],
-      annee: ['', rangeDateValidator(this.minDate, this.maxDate)],
-      fiche: [''],
+      types: [''],
+      date: ['', rangeDateValidator(this.minDate, this.maxDate)],
+      fiches: [''],
     });
+    // synchronous types
+    this.types = this.getTypes();
+    this.myForm.controls.types.patchValue(this.types[1].id);
 
-    this.myForm.patchValue({
-      fiche: 'courte'
-    });
+    this.fiches = this.getFiches();
+    this.myForm.controls.fiches.patchValue(this.fiches[1].id);
+  }
+  
+  ngOnInit() {
+    this.myForm.valueChanges
+      .subscribe(value => {
+        console.log('orderForm value changes : ', value);
+      });
+  }
 
+  getTypes() {
+    return [
+      { id: '1', type: 'film'},
+      { id: '2', type: 'série'},
+      { id: '3', type: 'épisode'}
+    ];
+  }
+
+  getFiches() {
+    return [
+      { id: '1', fiche: 'complète'},
+      { id: '2', fiche: 'courte'}
+    ]
   }
 
   onSubmit(response: FormGroup) {
-    response.valid ? console.log(this.myForm.value) : console.log('Invalid')
+    response.valid ? console.log('Valid') : console.log('Invalid')
   }
-
 }
